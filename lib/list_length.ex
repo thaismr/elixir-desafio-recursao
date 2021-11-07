736c566069f8643c7f667943d1e80f6df17ef1e3
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2021-11-07 16:51:22 -0300

    Desafio resolvido.

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: get_length(list, 0)
+
+  defp get_length([], acc), do: acc
+
+  defp get_length([_head | tail], acc) do
+    acc = acc + 1
+    get_length(tail, acc)
   end
 end
